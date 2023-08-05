e347e4e611c23146132feb8514397197e94b4e97
TanmayPShinde <tps.shinde@gmail.com>
2023-08-05 19:09:21 +0530

    fireabase setup

---
+++ src/app/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBMlwHhw5NiCY_lrSyqkxdKU125CWfNmAc",
+  authDomain: "midson-group.firebaseapp.com",
+  projectId: "midson-group",
+  storageBucket: "midson-group.appspot.com",
+  messagingSenderId: "421619005857",
+  appId: "1:421619005857:web:a7bdfadecf37e47e0533e0",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
