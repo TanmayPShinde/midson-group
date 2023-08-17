a900932f282ab0236085f654c90fde1bf4b749de
TanmayPShinde <tps.shinde@gmail.com>
2023-08-17 10:16:20 +0530

    feat: custom hook for useEffect, getAllCustomers

---
+++ src/hooks/useEffectCustomerOptions.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { getAllCustomers } from "../app/firebase";
+
+export default (setOptions) =>
+  useEffect(() => {
+    async function fetchData() {
+      // Fetch data
+      const data = await getAllCustomers();
+
+      // Store results in the results array
+      const options = data.map((option) => ({
+        label: option.name,
+        value: option.id,
+        email: option.email,
+      }));
+
+      // // Update the options state
+      setOptions(options);
+    }
+
+    // Trigger the fetch
+    fetchData();
+  }, []);
